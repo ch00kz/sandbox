6826766c1f221fc6887562957f78955f7d4876f3
Andrew Thomas <Andr3w.l.thomas@gmail.com>
2021-12-02 20:33:57 -0500

    testing vs code web

--- advent_of_code/2021/day_1.py
+++ advent_of_code/2021/day_1.py
@@ -1,1 +1,9 @@
+# suppose this is your 'test.py' file
+def main():
+ """This function runs the core of your program"""
+ print("running main")
 
+if __name__ == "__main__":
+ # if you call this script from the command line (the shell) it will
+ # run the 'main' function
+ main()
